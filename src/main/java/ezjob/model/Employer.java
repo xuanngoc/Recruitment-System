df24250110badeea8791de2bdb35d798ef010a47
pdc17499 <43161538+pdc17499@users.noreply.github.com>
2020-08-03 11:07:16 +0700

    tep

---
+++ src/main/java/ezjob/model/Employer.java
@@ -0,0 +1,72 @@
+package ezjob.model;
+
+public class Employer {
+	
+	private String company_name;
+	private String name;
+	private String email;
+	private String phone;
+	private String title;
+	private String city;
+	
+	
+	public String company_name() {
+		return company_name;
+	}
+
+	public void setCompany_name(String Company_name) {
+		this.company_name = Company_name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String Name) {
+		this.name = Name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String Email) {
+		this.email = Email;
+	}
+	
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String Phone) {
+		this.phone = Phone;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String Title) {
+		this.title = Title;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String City) {
+		this.city = City;
+	}
+
+
+
+
+	
+	
+	
+
+	
+	
+	
+
+}
---
+++ src/main/java/ezjob/repository/EmployerRepository.java
@@ -0,0 +1,5 @@
+package ezjob.repository;
+
+public interface EmployerRepository {
+
+}
