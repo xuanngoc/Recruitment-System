5780577059fbe9a9497f09ba4a3187a4a89a75e6
xuanngoc <bx.ngoc.99@gmail.com>
2020-08-24 21:25:42 +0700

    fix search query and fix applying cv feature

--- src/main/java/ezjob/service/ApplyingCVService.java
+++ src/main/java/ezjob/service/ApplyingCVService.java
@@ -1,10 +1,11 @@
 package ezjob.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import ezjob.model.ApplyingCV;
-import ezjob.model.Job;
 import ezjob.repository.ApplyingCVRepository;
 
 @Service
@@ -15,6 +16,14 @@ public class ApplyingCVService {
 	@Autowired
 	public void setAppyingCVRepository(ApplyingCVRepository appyingCVRepository) {
 		this.appyingCVRepository = appyingCVRepository;
+	}
+	
+	public String getPathFileCVById(long id) {
+		return appyingCVRepository.findPathFileCvById(id);
+	}
+	
+	public List<ApplyingCV> getApplyingCVsByJobId(long id) {
+		return appyingCVRepository.findByJobId(id);
 	}
 	
 	public void saveOrUpdate(ApplyingCV cv) {
